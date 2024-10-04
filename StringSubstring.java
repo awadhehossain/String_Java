47c197960de7a7f745bb302cc6275281b9f553a3
Awadhe Hossain <149267112+awadhehossain@users.noreply.github.com>
2024-10-05 00:56:50 +0600

    Add files via upload

---
+++ StringSubstring.java
@@ -0,0 +1,7 @@
+public class StringSubstring {
+    public static void main(String[] args) {
+        String s1 = "abcgXYZyqw";
+        String s2 = s1.substring(4,7);
+        System.out.println(s2);
+    }
+}
