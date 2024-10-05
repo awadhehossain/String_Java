76724defa3415a8a3d4dc2a9c9aa88b30bbe1c46
Awadhe Hossain <149267112+awadhehossain@users.noreply.github.com>
2024-10-05 10:41:31 +0600

    Update StringSplit.java

--- StringSplit.java
+++ StringSplit.java
@@ -2,7 +2,7 @@ public class StringSplit {
     public static void main(String[] args) {
         String s1 = "Abc,101,3.5"; //Abc || 101 || 3.5
 
-        String[] parts = s1.split("1");
+        String[] parts = s1.split(",");
         for(int i=0;i<parts.length;i++){
             System.out.println(parts[i]);
         }
